26a9b58316eb6a4fa1aec38ef66ce70bc08efb1e
Juan Antonio <montesgonzalezjuanantonio@gmail.com>
2021-12-16 11:09:41 +0100

    fibo version 1

---
+++ Fibonacci.py
@@ -0,0 +1,16 @@
+ n=int(input ("Valor máximo"))
+n1=0
+n2=1
+n3=0
+print(n1)
+print(n2)
+while(n3<n):          
+    n3=n1+n2
+    if n3<=n:
+     print (n3)
+    n1=n2
+    n2=n3
+   
+        
+      
+    
